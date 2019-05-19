7325668bca0b7cff7877a72882dabe7bd3b22a6b
zaroursamy <samy.zarour8430@gmail.com>
2019-05-20 00:38:51 +0200

    add tu ap

--- src/test/scala/api/ProbTest.scala
+++ src/test/scala/api/ProbTest.scala
@@ -1,5 +1,6 @@
 package api
 
+import api.Prob._
 import org.scalatest.FunSuite
 
 class ProbTest extends FunSuite {
@@ -18,6 +19,13 @@ class ProbTest extends FunSuite {
 
   test("testAp") {
 
+    val generateModulo: Prob[Double => Int] = new Prob[Double => Int] {
+      override def get: Double => Int = Bernoulli(0.5).mapp((d: Double) => math.round(d).toInt%2, (d:Double) => math.round(d).toInt%3).get
+    }
+
+    val apGenerateModulo: Prob[Int] = Uniform(0,10).ap(generateModulo)
+
+    Prob.sample(10)(apGenerateModulo).foreach(println)
   }
 
   test("testFilter") {
