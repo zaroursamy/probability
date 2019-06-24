909556c81d997c01b08cdbced2444e6ef1e7b9b0
zaroursamy <samy.zarour8430@gmail.com>
2019-06-25 00:33:26 +0200

    ++

--- src/test/scala/model/CoinTest.scala
+++ src/test/scala/model/CoinTest.scala
@@ -8,6 +8,14 @@ class CoinTest extends FunSuite {
   test("prob"){
 
     val coinProb = Bernoulli(0.5) to (Head, Tail)
+
+
+    println(coinProb.flatAndMap(c => if(c.isHead) Bernoulli(0.2) else Bernoulli(0.5)).density(identity, 10000))
+
+    println("$$$")
+
+    println(coinProb.flatMap(c => if(c.isHead) Bernoulli(0.2) else Bernoulli(0.5)).density(identity, 10000))
+
   }
 
 }
